f3272adf3cc328e605e5c919c5e2012beaab0a52
Oliver Ponder <oliver.ponder@gmail.com>
2015-04-29 09:10:42 +0200

    Extract the domain into a environment variable

--- simple_rails_app/start_rails.sh
+++ simple_rails_app/start_rails.sh
@@ -4,19 +4,21 @@ rake db:create db:setup
 
 IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 
-if (($(redis-cli -h frontend exists frontend:docker.dev) == 0))
+
+if (($(redis-cli -h frontend exists frontend:$DOMAIN) == 0))
 then
-  redis-cli -h frontend rpush frontend:docker.dev dockerdev
+  echo 'Frontend identifier doesnt exist. Creating'
+  redis-cli -h frontend rpush frontend:$DOMAIN dockerdev
 else
-  echo 'already exists'
+  echo 'The frontend identifier already exists. Skipping.'
 fi
 
-redis-cli -h frontend rpush frontend:docker.dev http://$IP:3000
+redis-cli -h frontend rpush frontend:$DOMAIN http://$IP:3000
 
 trap 'excode=$?; cleanup; echo $excode; exit' EXIT HUP INT QUIT PIPE TERM
 
 function cleanup {
-  redis-cli -h frontend lrem frontend:docker.dev 1 http://$IP:3000
+  redis-cli -h frontend lrem frontend:$DOMAIN 1 http://$IP:3000
 }
 
 rails server --port 3000 --binding 0.0.0.0 &
