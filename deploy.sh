5d3b753010367e120021cc2bf0d1cb2353af0b51
oliver-codes <oliver.ponder@gmail.com>
2015-05-02 03:59:20 +0200

    Add some wonky bash to sleep until the new backend cluster is ready

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,25 @@ docker-compose build
 
 if docker-compose ps | grep -q containersolutionsassignment_backenda
 then
-docker-compose scale backendb=3
-docker-compose scale backenda=0
+  docker-compose scale backendb=3
+
+  IP=$(docker ps | grep backendb | awk '{print $1}' | head -n 1 | xargs docker inspect | grep IPAddress | cut -d '"' -f 4)
+
+  until $(curl --output /dev/null --silent --head --fail http://$IP:3000); do
+    printf '.'
+    sleep 5
+  done
+
+  docker-compose scale backenda=0
 else
-docker-compose scale backenda=3
-docker-compose scale backendb=0
+  docker-compose scale backenda=3
+
+  IP=$(docker ps | grep backenda | awk '{print $1}' | head -n 1 | xargs docker inspect | grep IPAddress | cut -d '"' -f 4)
+
+  until $(curl --output /dev/null --silent --head --fail http://$IP:3000); do
+    printf '.'
+    sleep 5
+  done
+
+  docker-compose scale backendb=0
 fi
