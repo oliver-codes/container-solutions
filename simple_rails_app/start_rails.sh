d8961e5ec7bdd88bc2e3aeb1946d450c919e68a0
oliver-codes <oliver.ponder@gmail.com>
2015-05-02 13:52:57 +0200

    Adjust startup script again.

--- simple_rails_app/start_rails.sh
+++ simple_rails_app/start_rails.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 trap 'excode=$?; cleanup; echo $excode; exit' EXIT HUP INT QUIT PIPE TERM
 
 function cleanup {
@@ -10,14 +11,15 @@ if (($(redis-cli -h frontend exists frontend:$DOMAIN) == 0))
 then
   echo 'Frontend identifier doesnt exist. Creating'
   redis-cli -h frontend rpush frontend:$DOMAIN dockerdev
-  rake db:create db:setup
 else
   echo 'The frontend identifier already exists. Skipping.'
 fi
 
-IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-redis-cli -h frontend rpush frontend:$DOMAIN http://$IP:3000
+rake db:create db:setup
 
 rails server --port 3000 --binding 0.0.0.0 &
+
+IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+redis-cli -h frontend rpush frontend:$DOMAIN http://$IP:3000
 
 wait $1
