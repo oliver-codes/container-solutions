55b7f455aa5b009950af9e8d1473f87789127364
oliver-codes <oliver.ponder@gmail.com>
2015-05-02 13:40:04 +0200

    Clean up the deploy and startup script. Add a note about data persistence

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,22 @@
 git pull
 docker-compose build
 
+wait_till_backend_is_serving_requests() {
+  IP=$(docker ps | grep backend$1 | awk '{print $1}' | tail -n 1 | xargs docker inspect | grep IPAddress | cut -d '"' -f 4)
+
+  until $(curl --output /dev/null --silent --head --fail http://$IP:3000); do
+    printf '.'
+    sleep 5
+  done
+}
+
 if docker-compose ps | grep -q containersolutionsassignment_backenda
 then
   docker-compose scale backendb=3
-
-  IP=$(docker ps | grep backendb | awk '{print $1}' | head -n 1 | xargs docker inspect | grep IPAddress | cut -d '"' -f 4)
-
-  until $(curl --output /dev/null --silent --head --fail http://$IP:3000); do
-    printf '.'
-    sleep 5
-  done
-
+  wait_till_backend_is_serving_requests "b"
   docker-compose scale backenda=0
 else
   docker-compose scale backenda=3
-
-  IP=$(docker ps | grep backenda | awk '{print $1}' | head -n 1 | xargs docker inspect | grep IPAddress | cut -d '"' -f 4)
-
-  until $(curl --output /dev/null --silent --head --fail http://$IP:3000); do
-    printf '.'
-    sleep 5
-  done
-
+  wait_till_backend_is_serving_requests "a"
   docker-compose scale backendb=0
 fi
--- simple_rails_app/start_rails.sh
+++ simple_rails_app/start_rails.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-rake db:create db:setup
-
-IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-
-
-if (($(redis-cli -h frontend exists frontend:$DOMAIN) == 0))
-then
-  echo 'Frontend identifier doesnt exist. Creating'
-  redis-cli -h frontend rpush frontend:$DOMAIN dockerdev
-else
-  echo 'The frontend identifier already exists. Skipping.'
-fi
-
-redis-cli -h frontend rpush frontend:$DOMAIN http://$IP:3000
-
 trap 'excode=$?; cleanup; echo $excode; exit' EXIT HUP INT QUIT PIPE TERM
 
 function cleanup {
   redis-cli -h frontend lrem frontend:$DOMAIN 1 http://$IP:3000
 }
+
+if (($(redis-cli -h frontend exists frontend:$DOMAIN) == 0))
+then
+  echo 'Frontend identifier doesnt exist. Creating'
+  redis-cli -h frontend rpush frontend:$DOMAIN dockerdev
+  rake db:create db:setup
+else
+  echo 'The frontend identifier already exists. Skipping.'
+fi
+
+IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+redis-cli -h frontend rpush frontend:$DOMAIN http://$IP:3000
 
 rails server --port 3000 --binding 0.0.0.0 &
 
